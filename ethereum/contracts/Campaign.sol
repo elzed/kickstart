da6290dfcb259495a4ee25bf67a712ef0407b6c8
elzed <elzed@users.noreply.github.com>
2019-03-05 11:09:52 -0800

    Add contribute() func to allow ether contributers

--- ethereum/contracts/Campaign.sol
+++ ethereum/contracts/Campaign.sol
@@ -3,11 +3,23 @@ pragma solidity ^0.4.17;
 contract Campaign {
     address public manager;  // Define the manager
     uint public minimumContribution;
+    address[] public approvers;
 
     // Define the constructor function
     // Function sets the owner and the minimumContribution
     function Campaign(uint minimum) public {
         manager = msg.sender;
         minimumContribution = minimum;
+    }
+
+    // Call whenever someone wants to send money to contract
+    // Mark as `payable` any time money is sent
+    function contribute() public payable {
+
+        // Access the global variable `msg` to verify value in wei
+        require(msg.value > minimumContribution);
+
+        approvers.push(msg.sender);
+
     }
 }
