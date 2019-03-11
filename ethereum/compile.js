fc71e52906632661718df4b414f202ad1e04fda8
elzed <elzed@users.noreply.github.com>
2019-03-11 14:39:15 -0700

    Write output to the build directory

--- ethereum/compile.js
+++ ethereum/compile.js
@@ -12,9 +12,19 @@ const campaignPath = path.resolve(__dirname, 'contracts', 'Campaign.sol');
 const source = fs.readFileSync(campaignPath, 'utf8');
 
 
-// Contains two separate objects
-// Campaign Contract - Campaign Factory
-const output = solc.compile(source, 1).contracts
+// Contains two separate objects:
+// Campaign Contract and Campaign Factory
+const output = solc.compile(source, 1).contracts;
 
 
-// TODO: Write output to the build directory
+// Write output to the build directory
+fs.ensureDirSync(buildPath);  // make sure there's a directory at all
+
+for (let contract in output) {  // iterate over object props AKA campaign contracts
+    fs.outputJsonSync(
+        // Create absolute path and remove the solidity default of prefixing colon,
+        // Output the object we are iterating over into its named file
+        path.resolve(buildPath, contract.replace(':', '') + '.json'),
+        output[contract]
+    );
+}
