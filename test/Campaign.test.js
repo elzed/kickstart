617c29fb787f5d4a0f120a3c96772a17cdc1c842
elzed <elzed@users.noreply.github.com>
2019-03-12 16:43:22 -0700

    Add beforeEach() function and get list of accounts

--- test/Campaign.test.js
+++ test/Campaign.test.js
@@ -10,5 +10,23 @@ const web3 = new Web3(ganache.provider);
 const compiledFactory = require('../ethereum/build/CampaignFactory.json');
 const compiledCampaign = require('../ethereum/build/Campaign.json');
 
+
 // TODO: Create beforeEach() statement to deploy instance of compiledFactory
 // TODO: Access compiledFactory to create campaigns and write test around them
+
+let accounts;
+let factory;
+let campaignAddress;
+let campaign;
+
+beforeEach(async () => {
+    // Use provider to return a list of account the node controls
+    accounts = await web3.eth.getAccounts();
+
+    // TODO: 1. Deploy instance of compiledFactory contract
+    // TODO: 2. Use Contract() constructor from web3 library passing in compiledFactory ABI
+    // TODO: 3. Deploy and send transaction out to network
+
+    // compiledFactory ABI (Solidity <-> JavaScript),
+
+});
