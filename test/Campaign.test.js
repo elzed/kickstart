0c0e660caeb53baac831895fc96dfd83bf61bcf0
elzed <elzed@users.noreply.github.com>
2019-03-18 14:58:12 -0700

    Assert that the the initiator of contract is manager

--- test/Campaign.test.js
+++ test/Campaign.test.js
@@ -50,4 +50,9 @@ describe('Campaigns', () => {
         assert.ok(factory.options.address);
         assert.ok(campaign.options.address);
     });
+
+    it('should mark caller as the campaign manager', async () => {
+        const manager = await campaign.methods.manager().call();
+        assert.equal(accounts[0], manager);
+    });
 });
