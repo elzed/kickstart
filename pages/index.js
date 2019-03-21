56722fd96170bb65c642200aaf3e1dbb3af71655
elzed <elzed@users.noreply.github.com>
2019-03-21 13:49:03 -0700

    Retrieve and display list of deployed campaigns

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,17 @@
 // TODO: Use Factory instance to retrieve a list of deployed campaigns
 // TODO: Use React to show something about each campaign
 
-import React from 'react';
+import React, { Component } from 'react';
+import factory from '../ethereum/factory';
 
-export default () => {
-    return <h1>This is the campaign list page!!!</h1>;
+class CampaignIndex extends Component {
+    async componentDidMount() {
+        // Use CampaignFactory instance to call getDeployedCampaigns()
+        const campaigns = await factory.methods.getDeployedCampaigns().call();
+        console.log(campaigns);
+    }
+
+    render() {
+        return <div>Campaigns Index</div>
+    }
 }
