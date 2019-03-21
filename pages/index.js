6e342017b24c62321b725f2b14377ee83e1466fb
elzed <elzed@users.noreply.github.com>
2019-03-21 10:08:47 -0700

    Update TODOs

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,3 @@
-// TODO: Configure web3 with a provider from MetaMask
-// TODO: Tell web3 that a deployed copy of the 'CampaignFactory' exists
 // TODO: Use Factory instance to retrieve a list of deployed campaigns
 // TODO: Use React to show something about each campaign
 
