00521ac274d15d63741cdcb7a65b9fc98d272ecc
elzed <elzed@users.noreply.github.com>
2019-03-20 18:29:24 -0700

    Rename index.js and map out TODOs

---
+++ .next/dist/pages/index.js
@@ -0,0 +1,27 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var _jsxFileName = '/Users/lee/Documents/Learning/Grider/Ethereum-Solidity/kickstart/pages/index.js?entry';
+// TODO: Configure web3 with a provider from MetaMask
+// TODO: Tell web3 that a deployed copy of the 'CampaignFactory' exists
+// TODO: Use Factory instance to retrieve a list of deployed campaigns
+// TODO: Use React to show something about each campaign
+
+exports.default = function () {
+    return _react2.default.createElement('h1', {
+        __source: {
+            fileName: _jsxFileName,
+            lineNumber: 9
+        }
+    }, 'This is the campaign list page!!!');
+};
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInBhZ2VzL2luZGV4LmpzIl0sIm5hbWVzIjpbIlJlYWN0Il0sIm1hcHBpbmdzIjoiOzs7Ozs7QUFLQSxBQUFPLEFBRVA7Ozs7Ozs7QUFQQTtBQUNBO0FBQ0E7QUFDQTs7a0JBSWUsWUFBTSxBQUNqQjsyQkFBTyxjQUFBOztzQkFBQTt3QkFBQTtBQUFBO0FBQUEsS0FBQSxFQUFQLEFBQU8sQUFDVjtBQUZEIiwiZmlsZSI6ImluZGV4LmpzP2VudHJ5Iiwic291cmNlUm9vdCI6Ii9Vc2Vycy9sZWUvRG9jdW1lbnRzL0xlYXJuaW5nL0dyaWRlci9FdGhlcmV1bS1Tb2xpZGl0eS9raWNrc3RhcnQifQ==
---
+++ pages/index.js
@@ -0,0 +1,10 @@
+// TODO: Configure web3 with a provider from MetaMask
+// TODO: Tell web3 that a deployed copy of the 'CampaignFactory' exists
+// TODO: Use Factory instance to retrieve a list of deployed campaigns
+// TODO: Use React to show something about each campaign
+
+import React from 'react';
+
+export default () => {
+    return <h1>This is the campaign list page!!!</h1>;
+}
