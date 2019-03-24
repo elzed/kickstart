a50f8a0f7581a4c5a2a890e16d3b54277f978fe9
elzed <elzed@users.noreply.github.com>
2019-03-24 16:58:23 -0700

    Refactor with if...else to handle alt configurations
    If code is being executed inside the browser and MetaMask is
    available then, as before, hijack MetaMask's current provider and use
    it to create our own instance of Web3.
    Else, set up our own provider that connects to the Rinkeby Test network
    through Infura, and use that as the provider for our Web3 instance.

--- ethereum/web3.js
+++ ethereum/web3.js
@@ -1,9 +1,26 @@
 // Configure web3 with a provider from MetaMask
 
+
+// .gitIgnore-d accounts file
+const accountsInfo = require('./accounts');
+
 import Web3 from 'web3';
 
-// Assign the provider given to us from MetaMask
-// This presumes MetaMask has injected a provider into the page
-const web3 = new Web3(window.web3.currentProvider);
+let web3;
+
+// Verify code is being executed in the browser and MetaMask available
+// Else ...
+if (typeof window !== 'undefined' && typeof window.web3 !== 'undefined') {
+    // We are in the browser and MetaMask is running so we will
+    // use MetaMask's current provider
+    web3 = new Web3(window.web3.currentProvider);
+} else {
+    // We are on the server OR the user is not running MetaMask so we will
+    // use Infura to get a provider for our Web3 instance
+    const provider = new Web3.providers.HttpProvider(
+        accountsInfo.provider_url
+    );
+    web3 = new Web3(provider); // create our own instance of Web3
+}
 
 export default web3;
