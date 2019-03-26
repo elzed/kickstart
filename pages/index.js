7c5fa5049cf171e9478f7b66707dfaed50e99cd5
elzed <elzed@users.noreply.github.com>
2019-03-25 22:17:34 -0700

    Add TODOs for Next.js refactor

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import factory from '../ethereum/factory';
 
+// TODO: Refactor to move getDeployedCampaigns() out of componentDidMount()
+// TODO: because server-side rendering of the later does not occur in Next.js
 class CampaignIndex extends Component {
     async componentDidMount() {
         // Use CampaignFactory instance to call getDeployedCampaigns()
