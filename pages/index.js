363068b65c4c56881005736ea036992118e62036
elzed <elzed@users.noreply.github.com>
2019-04-02 14:31:46 -0700

    Quick fix to get styling via semantic-ui-react CDN

--- pages/index.js
+++ pages/index.js
@@ -27,7 +27,13 @@ class CampaignIndex extends Component {
     }
 
     render() {
-        return <div>{this.renderCampaigns()}</div>;
+        return <div>
+            <link
+                rel="stylesheet"
+                href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css"
+            />
+            {this.renderCampaigns()}
+        </div>;
     }
 }
 
