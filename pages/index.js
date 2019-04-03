1c8efb47cf0bbf127d00f654c58b4787cc0bbc62
elzed <elzed@users.noreply.github.com>
2019-04-03 15:09:40 -0700

    Move CSS CDN link to Layout file

--- pages/index.js
+++ pages/index.js
@@ -32,10 +32,6 @@ class CampaignIndex extends Component {
             <Layout>
                 {/*JSX passed to Layout component as children property*/}
                 <div>
-                    <link
-                        rel="stylesheet"
-                        href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css"
-                    />
                     <h3>Open Campaigns</h3>
                     <Button
                         floated="right"
