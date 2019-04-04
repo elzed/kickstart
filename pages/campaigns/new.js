c9af9b9b90aada5965daecb2b257f0030137e764
elzed <elzed@users.noreply.github.com>
2019-04-04 09:44:21 -0700

    Add state and event handler on the input

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -3,6 +3,10 @@ import { Form, Button, Input } from 'semantic-ui-react';
 import Layout from '../../components/Layout';
 
 class CampaignNew extends Component {
+    state = {
+        minimumContribution: ''
+    };
+
     render() {
         return (
             <Layout>
@@ -10,7 +14,13 @@ class CampaignNew extends Component {
                 <Form>
                     <Form.Field>
                         <label>Minimum Contribution</label>
-                        <Input label="wei" labelPosition="right" />
+                        <Input
+                            label="wei"
+                            labelPosition="right"
+                            value={this.state.minimumContribution}
+                            onChange={event =>
+                                this.setState({ minimumContribution: event.target.value })}
+                        />
                     </Form.Field>
                     <Button primary type='submit'>Create!</Button>
                 </Form>
