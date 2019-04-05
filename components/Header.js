123eba0c9081e574ccab21e52bf6e1b70033256d
elzed <elzed@users.noreply.github.com>
2019-04-05 09:18:33 -0700

    Add links to rightside navigations in Header

--- components/Header.js
+++ components/Header.js
@@ -6,13 +6,15 @@ export default () => {
     return (
         <Menu style={{ marginTop: '10px' }}>
             <Link route="/">
-                <a className="item">
-                    CrowdCoin
-                </a>
+                <a className="item">CrowdCoin</a>
             </Link>
             <Menu.Menu position="right">
-                <Menu.Item>Campaigns</Menu.Item>
-                <Menu.Item>+</Menu.Item>
+                <Link route="/">
+                    <a className="item">Campaigns</a>
+                </Link>
+                <Link route="/campaigns/new">
+                    <a className="item">+</a>
+                </Link>
             </Menu.Menu>
         </Menu>
     );
