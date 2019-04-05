e61cffa529f5bb96fc5127191eddaae4eacbe353
elzed <elzed@users.noreply.github.com>
2019-04-05 15:04:39 -0700

    Define a new route mapping with a wildcard

--- routes.js
+++ routes.js
@@ -1,5 +1,10 @@
 // Use IIFE syntax as per npm documentation
 const routes = require('next-routes')();
 
+routes
+    .add('/campaigns/new', '/campaigns/new')
+    // Define a new route mapping with wildcard
+    .add('/campaigns/:address', '/campaigns/show');
+
 // Export helpers to auto-navigate users through app
 module.exports = routes;
