648a2581f60f1c3df95cee6e911efdd829ab8172
elzed <elzed@users.noreply.github.com>
2019-04-06 17:35:13 -0700

    Update contract with getSummary() function

--- ethereum/contracts/Campaign.sol
+++ ethereum/contracts/Campaign.sol
@@ -117,4 +117,14 @@ contract Campaign {
 
         request.complete = true;
     }
+
+    function getSummary() public view returns (uint, uint, uint, uint, address) {
+        return (
+            minimumContribution,
+            this.balance,
+            requests.length,
+            approversCount,
+            manager
+        );
+    }
 }
