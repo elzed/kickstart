37eecf9e26fd0a356719382ba0e92f77c6d73c58
elzed <elzed@users.noreply.github.com>
2019-04-11 12:51:47 -0700

    Get address and return as prop for component

--- pages/campaigns/requests/new.js
+++ pages/campaigns/requests/new.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react';
+import { Form, Button, Message } from 'semantic-ui-react';
+import Campaign from '../../../ethereum/campaign';
+import web3 from '../../../ethereum/web3';
+import { Link, Router } from '../../../routes';
 
 class RequestNew extends Component {
+    // Get address and return as prop for the component
+    static async getInitialProps(props) {
+        const { address } = props.query;
+        return { address };
+    }
+
     render() {
         return <h3>Create a Request</h3>;
     }
