f08fc1451b4aabbf28724b68b89a37f2db0e4ba6
elzed <elzed@users.noreply.github.com>
2019-04-11 14:10:47 -0700

    Clean up method calls

--- pages/campaigns/requests/new.js
+++ pages/campaigns/requests/new.js
@@ -27,11 +27,9 @@ class RequestNew extends Component {
 
         try {
             const accounts = await web3.eth.getAccounts();
-            await campaign.methods.createRequest(
-                description,
-                web3.utils.toWei(value, 'ether'),
-                recipient
-            ).send({ from: account[0] })
+            await campaign.methods
+                .createRequest(description, web3.utils.toWei(value, 'ether'), recipient)
+                .send({ from: account[0] });
         } catch (err) {}
     };
 
