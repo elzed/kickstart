e801f1eaf70c2bd9448f2b8c0f7ca234496b5be5
elzed <elzed@users.noreply.github.com>
2019-04-10 20:44:06 -0700

    Add route to the Requests list

--- routes.js
+++ routes.js
@@ -4,7 +4,8 @@ const routes = require('next-routes')();
 routes
     .add('/campaigns/new', '/campaigns/new')
     // Define a new route mapping with wildcard
-    .add('/campaigns/:address', '/campaigns/show');
+    .add('/campaigns/:address', '/campaigns/show')
+    .add('/campaigns/:address/requests', '/campaigns/requests/index');
 
 // Export helpers to auto-navigate users through app
 module.exports = routes;
