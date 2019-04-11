452578dc422d872bf72b4a036b079e823b479ea0
elzed <elzed@users.noreply.github.com>
2019-04-11 14:44:30 -0700

    Add error handler and waiting spinner on button

--- pages/campaigns/requests/new.js
+++ pages/campaigns/requests/new.js
@@ -9,7 +9,9 @@ class RequestNew extends Component {
     state = {
         value: '',
         description: '',
-        recipient: ''
+        recipient: '',
+        loading: false,
+        errorMessage: ''
     };
 
     // Get address and return as prop for the component
@@ -25,19 +27,30 @@ class RequestNew extends Component {
         const campaign = Campaign(this.props.address);
         const { description, value, recipient } = this.state;
 
+        this.setState({ loading: true, errorMessage: '' });
+
         try {
             const accounts = await web3.eth.getAccounts();
             await campaign.methods
                 .createRequest(description, web3.utils.toWei(value, 'ether'), recipient)
                 .send({ from: accounts[0] });
-        } catch (err) {}
+
+            Router.pushRoute(`/campaigns/${this.props.address}/requests`);
+        } catch (err) {
+            this.setState({ errorMessage: err.message });
+        }
+
+        this.setState({ loading: false });
     };
 
     render() {
         return (
             <Layout>
+                <Link route={`/campaigns/${this.props.address}/requests`}>
+                    <a>Back</a>
+                </Link>
                 <h3>Create a Request</h3>
-                <Form onSubmit={this.onSubmit}>
+                <Form onSubmit={this.onSubmit} error={!!this.state.errorMessage}>
                     <Form.Field>
                         <label>Description</label>
                         <Input
@@ -65,7 +78,8 @@ class RequestNew extends Component {
                         />
                     </Form.Field>
 
-                    <Button primary>Create!</Button>
+                    <Message error header="Oops!" content={this.state.errorMessage} />
+                    <Button primary loading={this.state.loading}>Create!</Button>
                 </Form>
             </Layout>
         );
