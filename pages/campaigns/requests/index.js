210fb1e59ca206456e9c9410be42428388a9c7d5
elzed <elzed@users.noreply.github.com>
2019-04-12 14:24:31 -0700

    Import RequestRow and iterate list and render

--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -3,6 +3,7 @@ import { Button, Table } from 'semantic-ui-react';
 import { Link } from '../../../routes';
 import Layout from '../../../components/Layout';
 import Campaign from '../../../ethereum/campaign';
+import RequestRow from '../../../components/RequestRow';
 
 class RequestIndex extends Component {
     static async getInitialProps(props) {
@@ -21,6 +22,16 @@ class RequestIndex extends Component {
         );
 
         return { address, requests, requestCount };
+    }
+
+    renderRows() {
+        return this.props.requests.map((request, index) => {
+            return <RequestRow
+                key={index}  // Pass in key to render list of components
+                request={request}
+                address={this.props.address}
+            />;
+        });
     }
 
     render() {
@@ -46,6 +57,7 @@ class RequestIndex extends Component {
                             <HeaderCell>Finalize</HeaderCell>
                         </Row>
                     </Header>
+                    <Body>{this.renderRows()}</Body>
                 </Table>
             </Layout>
         );
