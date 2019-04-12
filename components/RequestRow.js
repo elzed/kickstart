269e5a9605d3ff62f650a783921f4562f06062a4
elzed <elzed@users.noreply.github.com>
2019-04-12 14:59:25 -0700

    Add id, description, and value amount to Table Rows

--- components/RequestRow.js
+++ components/RequestRow.js
@@ -1,8 +1,18 @@
 import React, { Component } from 'react';
+import { Table } from 'semantic-ui-react';
 
 class RequestRow extends Component {
     render() {
-        return <div>Request!</div>;
+        const { Row, Cell } = Table;
+        const { id, request } = this.props;
+
+        return (
+            <Row>
+                <Cell>{id}</Cell>
+                <Cell>{request.description}</Cell>
+                <Cell>{request.value}</Cell>
+            </Row>
+        );
     }
 }
 
